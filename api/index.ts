68512ef1ab84a9409ed600560bb2023811484a9e
WhyK <windchimeyk@gmail.com>
2020-12-15 00:30:36 +0900

    feat: add setHeader

--- api/index.ts
+++ api/index.ts
@@ -12,6 +12,8 @@ app.get('/api', (_, res) => {
 
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
   res.end(`Item: ${slug}`)
 })
 
